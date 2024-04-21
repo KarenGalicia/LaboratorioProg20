dea260ebfd3d4d82edf746f77521efe3339d3ea1
Karen Galicia <159975831+KarenGalicia@users.noreply.github.com>
2024-04-20 20:54:28 -0600

    Hola

--- Program.cs
+++ Program.cs
@@ -1,21 +1,13 @@
-﻿
+﻿using LaboratorioProg20.Laboratorio.Hija;
 
-using LaboratorioProg20.Laboratorio.Hija;
-
-
-//ejemplo de implementaciones:
-// Instanciamos un objeto de la clase JuegoAccion
 JuegoAccion juegoAccion = new JuegoAccion("Call of Duty: Modern Warfare", "Activision", 9.2, 2019, 5);
 juegoAccion.MostrarInformacion();
 
 
-// Instanciamos un objeto de la clase JuegoDeportes
-// Establecemos las propiedades del objeto JuegoDeportes
-JuegoDeportes juegoDeportes = new JuegoDeportes("FIFA 22", "EA Sports", 8.7, 2021, "Fútbol");
+JuegoDeportes juegoDeportes = new("FIFA 22", "EA Sports", 8.7, 2021, "Fútbol");
 juegoDeportes.MostrarInformacion();
 
 
-// Instanciamos un objeto de la clase JuegoShooter
-JuegoShooter juegoShooter = new JuegoShooter("Overwatch", "Blizzard Entertainment", 8.5, 2016, 200);
+JuegoShooter juegoShooter = new("Overwatch", "Blizzard Entertainment", 8.5, 2016, 200);
 
 juegoShooter.MostrarInformacion();
